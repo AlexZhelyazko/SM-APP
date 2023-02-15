1b51c072d0f2facfaa65100f372d6dd911641787
Alex <alex.zhelyazko@yandex.by>
2023-02-15 14:09:18 +0400

    add: register

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,5 +1,26 @@
 import './register.scss';
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>Some text</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
