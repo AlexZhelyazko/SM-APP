ca2e23b234c6a06f7a5d000205672c27b2276f3c
Alex <alex.zhelyazko@yandex.by>
2023-02-23 20:22:49 +0400

    add: register. later: backend

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,7 +1,33 @@
 import './register.scss';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 export const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+    name: '',
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      setErr(null);
+      const res = await axios.post('http://localhost:8800/api/auth/register', inputs);
+      console.log(res);
+    } catch (error: any) {
+      setErr(error.response.data);
+    }
+  };
+
+  console.log(inputs);
   return (
     <div className="register">
       <div className="card">
@@ -16,11 +42,27 @@ export const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={(e) => handleChange(e)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={(e) => handleChange(e)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => handleChange(e)}
+            />
+            <input type="text" placeholder="Name" name="name" onChange={(e) => handleChange(e)} />
+            {err && <div>{err}</div>}
+            <button onClick={(e) => handleClick(e)}>Register</button>
           </form>
         </div>
       </div>
