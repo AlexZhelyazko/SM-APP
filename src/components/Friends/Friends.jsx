5d9d8cd761188330ee3ce8978004b61649929fd9
Alex <alex.zhelyazko@yandex.by>
2023-03-04 22:07:14 +0400

    .

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import './Friends.scss';
+import { useContext } from 'react';
+import { useQuery } from 'react-query';
+import { makeRequest } from '../../axios';
+import { AuthContext } from '../../context/authContext';
 
 export const Friends = () => {
-  return <div>Friends</div>;
+  const { currentUser } = useContext(AuthContext);
+
+  const { isLoading, data: followings } = useQuery(['relationship'], () =>
+    makeRequest.get('/friends?followerUserId=' + currentUser.id).then((res) => {
+      return res.data;
+    }),
+  );
+
+  if (isLoading) return <div>Loading</div>;
+  return (
+    <div>
+      {followings.map((el) => {
+        return (
+          <div className="following">
+            <img
+              src={
+                el.profilePic ||
+                'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'
+              }
+              alt=""
+            />
+            <div className="followingInfo">
+              <div>{el.name}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
